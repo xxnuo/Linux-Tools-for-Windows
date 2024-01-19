ec9f522feb66d73d2e57b0f19c0e323c2ddd6d2e
xxnuo <54252779+xxnuo@users.noreply.github.com>
2024-01-19 15:55:34 +0800

    v1.0

---
+++ install.bat
@@ -0,0 +1,13 @@
+@echo mingit-busybox + busybox64u 
+@echo got all the linux tools on Windows
+
+@echo remember to add cmd dir to path
+
+@echo download busybox64u using windows 10 embed curl
+curl https://frippery.org/files/busybox/busybox64u.exe
+
+@echo install tools to cmd dir
+busybox64u.exe --install cmd
+
+@echo install MinGit to add git
+winget install Git.MinGit
